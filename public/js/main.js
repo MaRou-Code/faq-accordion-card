421055b1d6af77f67b9a563a94a1965099959898
MaRou <marioniroussel@gmail.com>
2021-05-16 17:39:07 +0400

    sass refactoring

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,8 @@ let panels = document.querySelectorAll('.faq__panel');
 
 function togglePanel(panel) {
   let openedPanel = document.querySelector('.faq__panel--opened');
-  if (openedPanel) {
-    if (openedPanel != panel) {
-      openedPanel.classList.remove('faq__panel--opened');
-    }
+  if (openedPanel && openedPanel != panel) {
+    openedPanel.classList.remove('faq__panel--opened');
   }
   panel.classList.toggle('faq__panel--opened');
 }
